fb0996c77e0e290044bb27fc75675563601bae1f
Andrey Khokhlov <Hohlov908@gmail.com>
2022-10-16 23:10:23 +0300

    in progress

--- KnightsRPGGame/Units/IAttackable.cs
+++ KnightsRPGGame/Units/IAttackable.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using KnightsRPGGame.Objects;
+using KnightsRPGGame.Shells;
 
 namespace KnightsRPGGame.Units
 {
     internal interface IAttackable
     {
-        void Attack(IList<IHealthable> objects);
+        void Attack(Shell shell, Vector<float> direction);
     }
 }
